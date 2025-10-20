c76c3bf45cffd1ce450a1fd1b9c76fae491fa2fa
hayrunnisaiyikosker <hniyikosker@gmail.com>
2025-10-20 22:14:24 +0300

    Refactor users table and add login_history table

--- Sign in-up.sql
+++ Sign in-up.sql
@@ -1,131 +1,32 @@
---
--- PostgreSQL database dump
---
-
-\restrict z7XvfWW4xdtPaYsKSWTa7SNNBw3KL5scFyBAdPLtJYUgPUN7TRgphNlRSwhYcaB
-
--- Dumped from database version 16.10
--- Dumped by pg_dump version 16.10
-
--- Started on 2025-10-19 23:43:03
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- TOC entry 216 (class 1259 OID 16583)
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.users (
-    id integer NOT NULL,
-    username character varying(50) NOT NULL,
-    email character varying(100) NOT NULL,
-    password_hash character varying(255) NOT NULL,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	password_hash TEXT NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
-ALTER TABLE public.users OWNER TO postgres;
+CREATE TRIGGER trigger_update_users_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
 
---
--- TOC entry 215 (class 1259 OID 16582)
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.users_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
-
---
--- TOC entry 4893 (class 0 OID 0)
--- Dependencies: 215
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-
---
--- TOC entry 4735 (class 2604 OID 16586)
--- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
-
---
--- TOC entry 4887 (class 0 OID 16583)
--- Dependencies: 216
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.users (id, username, email, password_hash, created_at) FROM stdin;
-1	testuser	test@example.com	hashing_sifre	2025-10-18 21:56:45.406878
-3	testuser2	test2@example.com	hashing2_sifre	2025-10-18 21:58:39.566196
-\.
-
-
---
--- TOC entry 4894 (class 0 OID 0)
--- Dependencies: 215
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.users_id_seq', 3, true);
-
-
---
--- TOC entry 4738 (class 2606 OID 16593)
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-
---
--- TOC entry 4740 (class 2606 OID 16589)
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- TOC entry 4742 (class 2606 OID 16591)
--- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_username_key UNIQUE (username);
-
-
--- Completed on 2025-10-19 23:43:04
-
---
--- PostgreSQL database dump complete
---
-
-\unrestrict z7XvfWW4xdtPaYsKSWTa7SNNBw3KL5scFyBAdPLtJYUgPUN7TRgphNlRSwhYcaB
-
+CREATE TABLE login_history(
+    id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL,
+	login_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	ip_address VARCHAR(45),
+	CONSTRAINT fk_user
+	    FOREIGN KEY (user_id)
+		REFERENCES users (id)
+		ON DELETE CASCADE		
+);
